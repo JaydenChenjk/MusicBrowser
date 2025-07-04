e33dc4887ae17065d91d5671b7b0b0209f06b60a
chenjiakang <cjk24@mails.tsinghua.edu.cn>
2025-07-04 19:15:16 +0800

    Added some notations

--- musicbrowser/wsgi.py
+++ musicbrowser/wsgi.py
@@ -1,12 +1,6 @@
-"""
-WSGI config for musicbrowser project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicbrowser.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicbrowser.settings')    #  设置环境变量指向项目配置文件settings.py
 
-application = get_wsgi_application()
+application = get_wsgi_application()    # 构造符合 WSGI 规范的 application 对象
